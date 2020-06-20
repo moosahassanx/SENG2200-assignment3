d0da3a254f38129b90884b608a4e492af3ce7aca
Moosa Hassan <moosa7021@gmail.com>
2020-06-20 16:52:53 +1000

    need to figure out how to actually start implementing this

--- BeginningStage.java
+++ BeginningStage.java
@@ -6,6 +6,7 @@ public class BeginningStage {
     private int range;
 
     private Item car;
+    private int numberOfItems;
 
     // default constructor
     public BeginningStage(){
@@ -16,10 +17,15 @@ public class BeginningStage {
     }
 
     // main constructor
-    public BeginningStage(String n, int m, int r){
+    public BeginningStage(String n, int m, int r, int amount, String AorB){
         name = n;
         mean = m;
         range = r;
+        numberOfItems = amount;
+
+        for(int i = 0; i < numberOfItems; i++){
+            car = new Item(AorB);
+        }
     }
 
     // fake toString() method
@@ -27,7 +33,7 @@ public class BeginningStage {
         String printer = "";  
 
         //      stage name          work[%]             starve[t]           block[t]
-        printer += name + "\t\t" + "99.87%" + "\t\t" + "4,672.64" + "\t" + "48,409.24" + "\n";
+        printer += name + "\t\t" + "00.00%" + "\t\t" + "0,000.00" + "\t" + "00,000.00" + "\n";
 
         return printer;
     }
--- Item.java
+++ Item.java
@@ -5,32 +5,26 @@ public class Item {
     private int timeLeft;
     private String uniqueID;
     private Data[] dataArray;
+
+    public Item(){
+        timeEntered = 0;
+        timeLeft = 0;
+        uniqueID = "";
+    }
     
-    public Item()
+    public Item(String AorB)
     {
-        uniqueID = "ITEMID" + determineStart();
-        System.out.println("uniqueID: " + uniqueID);
+        uniqueID = "ITEMID" + AorB;
         
+        /*
         char startPoint = uniqueID.charAt(6);
         System.out.println("startPoint: " + startPoint);
+        */
     }
 
-    public void newData(String s, double tE, double tL){
+    public void newData(String s, double tE, double tL) {
         dataArray = new Data[1];
 
         dataArray[0] = new Data(s, tE, tL);
-    }
-
-    // randomly select if item is a or b
-    public String determineStart(){
-        Random r = new Random();
-        double AorB = r.nextDouble();
-
-        if(AorB < 0.5){
-            return "a";
-        }
-        else{
-            return "b";
-        }
     }
 }
